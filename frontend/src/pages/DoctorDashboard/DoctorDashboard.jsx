0e0f2d0cd3cc1463ef15e81857823cf6c425a7c7
thanurarukshan <thanurarukshan2000@gmail.com>
2025-08-16 23:15:46 +0530

    React Routing Done

---
+++ frontend/src/pages/DoctorDashboard/DoctorDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './DoctorDashboard.css'
+
+function DockerDashboard() {
+  return (
+    <div>DockerDashboard</div>
+  )
+}
+
+export default DockerDashboard
---
+++ frontend/src/pages/PatientDashboard/PatientDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PatientDashboard.css'
+
+function PatientDashboard() {
+  return (
+    <div>PatientDashboard</div>
+  )
+}
+
+export default PatientDashboard
